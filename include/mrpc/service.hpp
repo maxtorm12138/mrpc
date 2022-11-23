b37abffd12b43bb86716a1366392606cde20ae2d
maxzwliao <maxzwliao@tencent.com>
2022-11-23 19:03:10 +0800

    add implement

--- include/mrpc/service.hpp
+++ include/mrpc/service.hpp
@@ -1,12 +1,34 @@
 #ifndef MRPC_SERVICE_H
 #define MRPC_SERVICE_H
 
+#include <unordered_map>
+#include <format>
+
+// mrpc
 #include <mrpc/method.hpp>
+#include <mrpc.pb.h>
 
 namespace mrpc {
 
 class service : public boost::noncopyable
-{};
+{
+public:
+    service();
+
+    template<is_method Method, typename Implement>
+    service &implement(Implement implement);
+
+private:
+    std::unordered_map<uint8_t, std::unique_ptr<abstract_method>> methods_;
+};
+
+template<is_method Method, typename Implement>
+service &service::implement(Implement implement)
+{
+    uint64_t command_id = Method::descriptor()->options().GetExtension(cmd_id);
+    auto [iter, exists] = methods_.emplace(command_id, std::make_unique<abstract_method>(new method<Method, Implement>(std::move(implement))));
+    return *this;
+}
 
 } // namespace mrpc
 
