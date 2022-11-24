9bb765644455bc7eec5ae3dafbc1772768f48b8a
maxzwliao <maxzwliao@tencent.com>
2022-11-24 18:47:59 +0800

    test

--- test/test_packet_handler.cpp
+++ test/test_packet_handler.cpp
@@ -3,34 +3,60 @@
 #include <boost/asio.hpp>
 namespace net = boost::asio;
 
-net::awaitable<void> co_main()
+class PacketHandlerTest : public testing::Test
 {
-    mrpc::channel_packet_handler<std::vector<uint8_t>> handler(co_await net::this_coro::executor);
 
-    std::string msg = "hello from other side";
-    co_await handler.deliver(net::buffer(msg));
+protected:
+    void SetUp() override
+    {
+        io_context_ = std::make_unique<net::io_context>();
+    }
 
-    std::string received_msg;
-    auto buffer_received_msg = net::dynamic_buffer(received_msg);
+    void TearDown() override
+    {
+        io_context_ = nullptr;
+    }
 
-    co_await handler.receive(buffer_received_msg);
-    EXPECT_EQ(received_msg, msg);
+    void RunEntry(net::awaitable<void> entry)
+    {
+        net::co_spawn(io_context_->get_executor(), std::move(entry), [](std::exception_ptr ex) {
+            if (ex != nullptr)
+            {
+                std::rethrow_exception(ex);
+            }
+        });
+    }
 
-    std::string msg1 = "At least I can say that I've tried";
-    co_await handler.send(net::buffer(msg1));
+    std::unique_ptr<net::io_context> io_context_;
+};
 
-    std::string received_msg1;
-    auto buffer_received_msg1 = net::dynamic_buffer(received_msg1);
-    co_await handler.retain(buffer_received_msg1);
-    EXPECT_EQ(received_msg1, msg1);
-
-    EXPECT_FALSE(handler.in_.ready());
-    EXPECT_FALSE(handler.out_.ready());
-}
-
-int main()
+TEST_F(PacketHandlerTest, ChannelSend)
 {
-    mrpc::net::io_context io_context;
-    net::co_spawn(io_context, co_main(), net::detached);
-    io_context.run();
+    auto tester = []() -> net::awaitable<void> {
+        mrpc::channel_packet_handler<std::vector<uint8_t>> handler(co_await net::this_coro::executor);
+
+        std::string msg = "hello from other side";
+        co_await handler.deliver(net::buffer(msg));
+
+        std::string received_msg;
+        auto buffer_received_msg = net::dynamic_buffer(received_msg);
+
+        co_await handler.receive(buffer_received_msg);
+        EXPECT_EQ(received_msg, msg);
+
+        std::string msg1 = "At least I can say that I've tried";
+        co_await handler.send(net::buffer(msg1));
+
+        std::string received_msg1;
+        auto buffer_received_msg1 = net::dynamic_buffer(received_msg1);
+        co_await handler.retain(buffer_received_msg1);
+        EXPECT_EQ(received_msg1, msg1);
+
+        EXPECT_EQ(handler.in_.capacity(), 100);
+        EXPECT_EQ(handler.out_.capacity(), 100);
+
+        EXPECT_FALSE(handler.in_.ready());
+        EXPECT_FALSE(handler.out_.ready());
+    };
+    RunEntry(tester());
 }
